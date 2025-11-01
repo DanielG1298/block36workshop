e71cd9fc99b342504ef1225d8a3243c4b73deb1d
DanielG1298 <glaser.daniel1204@gmail.com>
2025-11-01 14:52:43 -0500

    workshop done

---
+++ submission.sql
@@ -0,0 +1,15 @@
+--SELECT * FROM forum_posts WHERE date BETWEEN '2048-04-01' AND '2048-05-01' AND content ILIKE '%dad%';--
+--username smart-money-44--
+--SELECT * FROM forum_accounts WHERE username LIKE 'smart-money-44';--
+--full name is brad Steele--
+--SELECT * FROM forum_accounts WHERE last_name LIKE 'Steele';--
+--sharp-engine-57 | Andrew, stinky-tofu-98  | Kevin,smart-money-44  | Brad--
+--SELECT * from emptystack_accounts WHERE last_name LIKE 'Steele';--
+--triple-cart-38 | password456 | Andrew, lance-main-11  | password789 | Lance--
+--SELECT * FROM emptystack_messages WHERE body ILIKE '%taxi%';--
+--LidWj | your-boss-99 | triple-cart-38 | Project TAXI | Deploy Project TAXI by end of week. We need this out ASAP.--
+--SELECT * FROM emptystack_accounts WHERE username LIKE 'your-boss-99';--
+--your-boss-99 | notagaincarter | Skylar     | Singer--
+--SELECT *FROM  emptystack_projects WHERE code LIKE 'TAXI';--
+--DczE0v2b | TAXI--
+--porject has been shutdown--
